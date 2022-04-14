8ac5e616de516d6b672b51f8cdd2626080b2fdee
RichBat <38309949+RichBat@users.noreply.github.com>
2022-04-14 18:41:02 +0200

    Update GraphMaker.py
    Adjusted to determine the intensity of the rescaled voxels per intensity across precision values

--- GraphMaker.py
+++ GraphMaker.py
@@ -7,6 +7,7 @@ from skimage import data, io
 import pandas as pd
 from os import listdir
 from os.path import isfile, join, exists
+from skimage.filters import apply_hysteresis_threshold
 
 manual_Hysteresis = {"CCCP_1C=0.tif": [[0.1, 0.408], [0.1, 0.25]], "CCCP_1C=1.tif": [[0.116, 0.373], [0.09, 0.22]],
                      "CCCP_2C=0.tif": [[0.107, 0.293], [0.09, 0.2]], "CCCP_2C=1.tif": [[0.09, 0.372], [0.08, 0.15]],
@@ -115,6 +116,32 @@ def collate_data(input_paths, image_paths=None):
                                         break
                                 if var_val is None:
                                     variants_results[var] = min(distances, key=distances.get)
+                            elif var == "Voxels per intensity" and type(var_val) is list:
+                                converted_values = []
+                                if len(var_val) > 1:
+                                    for l in var_val:
+                                        converted_values.append(float(l))
+                                    variants_results[var] = converted_values
+                                elif len(var_val) == 1:
+                                    if type(var_val[0]) is list:
+                                        for l in var_val[0]:
+                                            converted_values.append(float(l))
+                                        variants_results[var] = converted_values
+                                else:
+                                    variants_results[var] = []
+                            elif var == "Voxels changes" and type(var_val) is list:
+                                converted_values = []
+                                if len(var_val) > 1:
+                                    for l in var_val:
+                                        converted_values.append(float(l))
+                                    variants_results[var] = converted_values
+                                elif len(var_val) == 1:
+                                    if type(var_val[0]) is list:
+                                        for l in var_val[0]:
+                                            converted_values.append(float(l))
+                                        variants_results[var] = converted_values
+                                else:
+                                    variants_results[var] = []
                             elif var == "Intensity Range" and var_val is None:
                                 if img is not None:
                                     variants_results[var] = get_intensity_range(img, v["Low_Thresh"], v["Starting Density"], v["Precision"])
@@ -143,7 +170,8 @@ def save_data(save_path, sample_specific_metrics, sample_variant_metrics):
         by_density[sam_var["Starting Density"]].append({"Low_Thresh":sam_var["Low_Thresh"], "High_Thesh":sam_var["High_Thresh"],
                                                         "Precision":sam_var["Precision"], "Sample":sam_var["Sample"]})
         by_intensity.append({"High_Thesh":sam_var["High_Thresh"], "Intensity Range":sam_var["Intensity Range"],
-                             "Voxels per intensity":sam_var["Voxels per intensity"], "Total Voxels":sample_specific_metrics[sam_var["Sample"]]["Total Voxels"],
+                             "Voxels per intensity":sam_var["Voxels per intensity"], "Voxel Changes":sam_var["Voxels changes"],
+                             "Total Voxels":sample_specific_metrics[sam_var["Sample"]]["Total Voxels"],
                              "Precision":sam_var["Precision"], "Starting Density":sam_var["Starting Density"], "Filter Type":sam_var["Filter Type"], "Sample":sam_var["Sample"]})
         by_error.append({"Sample":sam_var["Sample"], "Richard Error":sam_var["Richard Error"], "Rensu Error":sam_var["Rensu Error"],
                          "Filter Type":sam_var["Filter Type"], "Starting Density":sam_var["Starting Density"], "Precision":sam_var["Precision"]})
@@ -315,16 +343,126 @@ def intensity_graphs(intensity_dict_list, filter, dens):
         plt.legend()
         plt.show()
 
+def intensity_graphs2(intensity_dict_list, filter, dens):
+    intensities_by_sample = {}
+    for i in intensity_dict_list:
+        if i["Sample"] not in intensities_by_sample:
+            intensities_by_sample[i["Sample"]] = []
+        if i["Filter Type"] == filter and i["Starting Density"] == dens:
+            intensities_by_sample[i["Sample"]].append({"Precision": i["Precision"], "Total Voxels": i["Total Voxels"],
+                                                       "Voxels per intensity": i["Voxels per intensity"], "Intensity Range": i["Intensity Range"],
+                                                       "Voxel Changes": i["Voxel Changes"], "High_Thesh": i["High_Thesh"]})
 
+    for k, v in intensities_by_sample.items():
+        #plt.clf()
+        print(k)
+        fig, (ax1, ax2, ax3) = plt.subplots(3)
+        fig.suptitle(k)
+        gradient_threshes = []
+        print(v)
+        high_thresh_set = {}
+        scaled_vox_average_set = {}
+        scaled_vox_indexes = {}
+        for j in v:
+            intens_range = j["Intensity Range"]
+            voxel_range = j["Voxels per intensity"]
+            changes_range = j["Voxel Changes"]
+            total_voxels = j["Total Voxels"]
+            measured_total = sum(voxel_range)
+            print(j["Precision"])
+            print("Measured vs Actual Total:", measured_total, total_voxels)
+            print("Provided High Thresh:", j["High_Thesh"])
+            print("***************************************************")
+            print("Number of intensity values:", len(intens_range))
+            print(intens_range, "\n", voxel_range, "\n", changes_range, "\n")
+            print("***************************************************")
+            max_intensity = 255
+            high_thresh_set[str(j["Precision"])] = j["High_Thesh"]
+            shorter_range = []
+            rescaled_voxel_diffs = []
+            for t in range(0, len(intens_range)):
+                if t == 0:
+                    prior_step = 255
+                else:
+                    prior_step = intens_range[t - 1]
+                current_step = intens_range[t]
+                rescaled_voxel_diffs.append(voxel_range[t]/(prior_step - current_step))
+            print("Rescaled Vox Differences:", rescaled_voxel_diffs)
+            calculated_changes = []
+            for s in range(1, len(intens_range)):
+                calculated_changes.append((voxel_range[s]/voxel_range[s - 1]))
+                shorter_range.append(intens_range[s])
+            accumulated_range = []
+            calculated_average = sum(calculated_changes[1:])/len(shorter_range[1:])
+            total = 0
+            average_change = sum(changes_range)/len(shorter_range)
+            print("Calculated Voxel Changes:", calculated_changes)
+            print(len(calculated_changes) == len(shorter_range))
+            print("Average Change:", average_change, "Calculated Average:", calculated_average)
+            for c in voxel_range:
+                total += c
+                accumulated_range.append(total)
+            other_change_calc = []
+            for a in range(1, len(accumulated_range)):
+                other_change_calc.append((accumulated_range[a]/accumulated_range[a - 1]) - 1)
+            print("Accumulated Change Difference:", other_change_calc)
+            intens_range.reverse()
+            scaled_vox_average_index = 1
+            scaled_vox_average = sum(rescaled_voxel_diffs) / len(rescaled_voxel_diffs)
+            rescaled_voxel_diffs.reverse()
+            for resc in range(len(rescaled_voxel_diffs) - 1, 0, -1):
+                if rescaled_voxel_diffs[resc] >= scaled_vox_average:
+                    scaled_vox_average_index = resc
+                    scaled_vox_indexes[str(j["Precision"])] = intens_range[scaled_vox_average_index]
+                    print("Rescaled Voxel Index", rescaled_voxel_diffs[resc], resc)
+                    break
+            print("Intensity Range:", intens_range)
+            print("Scaled Index:", scaled_vox_average_index)
+            print("Scaled voxel values:", rescaled_voxel_diffs)
+            print("Scaled Average:", scaled_vox_average)
+            scaled_vox_average_set[str(j["Precision"])] = scaled_vox_average
+            voxel_range.reverse()
+            changes_range.reverse()
+            shorter_range.reverse()
+            accumulated_range.reverse()
+            high_thresh_voxels = intens_range.index(j["High_Thesh"]) - 1
+            print("High Thresh Index:", high_thresh_voxels)
+            ax1.plot(intens_range, voxel_range, '-D', markevery=high_thresh_voxels, label=str(j["Precision"]))
+            ax2.plot(intens_range, rescaled_voxel_diffs, '-D', markevery=scaled_vox_average_index, label=str(j["Precision"]))
+            ax3.plot(intens_range, rescaled_voxel_diffs, '-D', label=str(j["Precision"]))
+        print("High Thresholds:", high_thresh_set)
+        print("Average Rescaled Voxels:", scaled_vox_average_set)
+        print("Average Rescaled Intensities:", scaled_vox_indexes)
+        print("Average Rescaled Intensity:", sum(list(scaled_vox_indexes.values()))/len(list(scaled_vox_indexes)))
+        set_of_values = set(list(high_thresh_set.values()))
+        print("Average High Thresh:", sum(list(set_of_values))/len(list(set_of_values)))
+        ax1.set_title("Voxels per intensity")
+        ax2.set_title("Rescaled Voxels per intensity")
+        ax3.set_title("Rescaled Voxels per intensity every point")
+        plt.legend()
+        plt.show()
+
+def thresholded_image_view(img_path, sample_name, low_thresh, high_thresh):
+    img = io.imread(img_path + sample_name.split(".")[0] + "Noise000" + ".tif")
+    thresholded_image = apply_hysteresis_threshold(img, low_thresh, high_thresh)*255
+    thresholded_image2 = apply_hysteresis_threshold(img, 25.5, 104.04) * 255
+    for slice in range(thresholded_image.shape[0]):
+        print("Slice:", slice)
+        io.imshow(thresholded_image[slice])
+        plt.show()
+        plt.clf()
+        io.imshow(thresholded_image2[slice])
+        plt.show()
 
 if __name__ == "__main__":
     input_path = "C:\\RESEARCH\\Mitophagy_data\\Testing Output 2\\"
     correct_path = "C:\\RESEARCH\\Mitophagy_data\\Testing Data Results\\"
-    sample_specific_metrics, sample_variant_metrics = collate_data(input_path, {"C:\\RESEARCH\\Mitophagy_data\\Testing Output\\":"C:\\RESEARCH\\Mitophagy_data\\Testing Input data\\"})
-    save_data(correct_path, sample_specific_metrics, sample_variant_metrics)
-    #loaded_data = load_data(correct_path)
-    #intensity_dict = loaded_data["Intensity"]
-    #intensity_graphs(intensity_dict, "Mean", 1)
+    #sample_specific_metrics, sample_variant_metrics = collate_data(input_path, {"C:\\RESEARCH\\Mitophagy_data\\Testing Output 2\\":"C:\\RESEARCH\\Mitophagy_data\\Testing Input data\\"})
+    #save_data(correct_path, sample_specific_metrics, sample_variant_metrics)
+    loaded_data = load_data(correct_path)
+    intensity_dict = loaded_data["Intensity"]
+    intensity_graphs2(intensity_dict, "Mean", 1)
+    #thresholded_image_view("C:\\RESEARCH\\Mitophagy_data\\Testing Input data 2\\", "CCCP_1C=0.tif", 53, 173)
     #filters = loaded_data["Filter"]
     #errors = loaded_data["Error"]
     #filter_graphing(filters, errors)
