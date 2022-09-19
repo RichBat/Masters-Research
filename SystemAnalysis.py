9e512a455c2d4764032f58c08d175db3cad0348a
RichBat <38309949+RichBat@users.noreply.github.com>
2022-09-19 18:44:34 +0200

    Update SystemAnalysis.py
    Built on low threshold analysis to organize data for Pandas viewing.

--- SystemAnalysis.py
+++ SystemAnalysis.py
@@ -18,6 +18,7 @@ from CleanThresholder import AutoThresholder
 class thresholding_metrics(AutoThresholder):
 
     def __init__(self, input_paths, deconv_paths=None, expert_path=None):
+        AutoThresholder.__init__(self, input_paths, deconv_paths)
         self.low_thresholds = {}
         self.expert_files = self._extract_expert_files(expert_path)
         self.exp_threshes = None
@@ -25,7 +26,6 @@ class thresholding_metrics(AutoThresholder):
         self._initialise_expert_info()
         if expert_path is not None:
             self._extract_exp_threshes()
-        AutoThresholder.__init__(self, input_paths, deconv_paths)
 
     def _prepare_image(self, image, filename):
         gray_image = self._grayscale(image)
@@ -57,7 +57,8 @@ class thresholding_metrics(AutoThresholder):
     def _extract_exp_threshes(self):
         '''
         This function will iterate through the expert threshold results and scrape the sample specific results, this results will be stored as a list of tuple
-        pairs for (low, high) thresholds. Not all experts evaluated all of the samples thus this check alleviates that.
+        pairs for (low, high) thresholds. Not all experts evaluated all of the samples thus this check alleviates that. This may need to be adjusted to include
+        the ranking since anonymising the expert values could affect the rank pairings unless in the expert ranking this is included.
         :return:
         '''
         expert_threshold_dicts = []  # This will be a compilation of all of the thresholding dictionaries from the different expert files
@@ -65,13 +66,25 @@ class thresholding_metrics(AutoThresholder):
             with open(threshold_files, "r") as j:
                 expert_threshold_dicts.append(json.load(j))
         for sample_thresholds in list(self.exp_threshes):
+            expert_none_count = 0
             for etd in expert_threshold_dicts:
+                if self.exp_threshes[sample_thresholds] is None:
+                    self.exp_threshes[sample_thresholds] = []
                 if sample_thresholds in etd:
-                    if self.exp_threshes[sample_thresholds] is None:
-                        self.exp_threshes[sample_thresholds] = []
                     self.exp_threshes[sample_thresholds].append(self._expert_threshold_dict_check(etd[sample_thresholds]))
+                else:
+                    self.exp_threshes[sample_thresholds].append(None)  # This will account for some experts not evaluating a sample
+                    expert_none_count += 1
+            if expert_none_count == len(self.exp_threshes[sample_thresholds]):
+                self.exp_threshes[sample_thresholds] = None
 
     def _expert_threshold_dict_check(self, value_dict):
+        '''
+        This function scrapes through the expert dictionary provided and searches for the sample. If the sample is present and so are both a low and high
+        threshold value then the tuple of the threshold values will be returned. If the sample was not reviewed by the expert then None is returned.
+        :param value_dict: The sample dictionary which is a child of the expert dictionary
+        :return:
+        '''
         if len(list(value_dict)) == 2 and "low" in value_dict and "high" in value_dict:
             low_value = value_dict["low"] if type(value_dict["low"]) is int or type(value_dict["low"]) is float else float(value_dict["low"])
             high_value = value_dict["high"] if type(value_dict["high"]) is int or type(value_dict["high"]) is float else float(value_dict["high"])
@@ -79,12 +92,14 @@ class thresholding_metrics(AutoThresholder):
             return thresh_values
         return None
 
-    def _compare_with_experts(self, sample_name, values, choice=0):
+    def _compare_with_experts(self, sample_name, values, choice=2):
         '''
         This method will receive the sample name to select the respective sample from the expert data, determine if it is present, and compare to the auto
-        results. The choice parameter is either '0' (both if list/tuple or low thresh), '1' for low thresh and '2' for high thresh. A distance from the expert
+        results. The choice parameter is either '2' (both if list/tuple or low thresh), '0' for low thresh and '1' for high thresh. A distance from the expert
         low threshold will be provided. If there are multiple expert values then an average of the distances and the distance from the average expert value
         will be provided. If only one expert value is present then a single value will be returned as a float.
+
+        This might be adjusted in future to take expert ranking into account.
         :param sample_name:
         :param values:
         :param choice:
@@ -93,7 +108,118 @@ class thresholding_metrics(AutoThresholder):
         if self.exp_threshes[sample_name] is None:
             return None
         else:
-            choice_indexes = []
+            expert_values = self.exp_threshes[sample_name]
+            comparison_results = []
+            if choice != 2 or len(values) == 1:
+                provided_value = values if (type(values) is not tuple or type(values) is not list) else values[choice]
+                if choice == 2:
+                    # defaults choice to 0 if only one value (low or high) has been provided thus low is assumed
+                    choice = 0
+                ev_collection = 0
+                ev_distances = 0
+                not_none_ev = 0
+                for ev in expert_values:
+                    if ev is not None:
+                        comparison_results.append(provided_value-ev[choice])
+                        ev_collection += ev[choice]
+                        ev_distances += provided_value-ev[choice]
+                        not_none_ev += 1
+                    else:
+                        comparison_results.append(None)
+                if len(comparison_results) > 1:
+                    average_expert = ev_collection/not_none_ev
+                    average_distance = ev_distances/not_none_ev
+                    comparison_results.append('mean')
+                    comparison_results.append([average_expert, average_distance])
+                return comparison_results
+            else:
+                provided_value = values
+                ev_collection = [0, 0]
+                ev_distances = [0, 0]
+                not_none_ev = 0
+                for ev in expert_values:
+                    if ev is not None:
+                        comparison_results.append((provided_value[0]-ev[0], provided_value[1]-ev[1]))
+                        ev_collection[0] += ev[0]
+                        ev_collection[1] += ev[1]
+                        ev_distances[0] += provided_value[0] - ev[0]
+                        ev_distances[1] += provided_value[1] - ev[1]
+                        not_none_ev += 1
+                    else:
+                        comparison_results.append(None)
+                if len(expert_values) > 1:
+                    comparison_results.append('mean')
+                    comparison_results.append([(ev_collection[0]/not_none_ev, ev_collection[1]/not_none_ev),
+                                               (ev_distances[0]/not_none_ev, ev_distances[1]/not_none_ev)])
+                return comparison_results
+
+    def _expert_ranking_weight(self, ranking, sample_name, expert_index):
+        '''
+        This function will be used to weight the deviation between the expert result and the automated result if the ranking is present. Since the thresholds
+        are anonymised between experts for threshold aggregation the associated rankings need to be tracked. Either the threshold extraction needs to
+        correctly number the associated ranking or include it in the self.exp_threshes that will be a tuple of (low, high, rank) where a value of None can be
+        provided if there is no rank. The method used to weight the rankings will be based on the numeric weighting where 3 is the centre value and a rank
+        lower than 3 (<3) will be worse .
+        :param ranking:
+        :param sample_name:
+        :param expert_index:
+        :return:
+        '''
+        print("none")
+
+    def _low_thresh_compare(self, value_sequence, sample_name):
+        '''
+        This will iterate through the provided low thresholds (knee types, Otsu, etc.) and evaluate the distance from the experts. This will be used to feed
+        an organized dictionary for all samples to be aggregated.
+        :param value_sequence: dict of values with measurement type as key
+        :return:
+        '''
+        distance_results = {}
+        total_expert_count = 1
+        for thresh_type, thresh_value in value_sequence.items():
+            expert_count = 1  # this will be used to track the number of experts for a sample
+            distances = self._compare_with_experts(sample_name, thresh_value, choice=0)  # can be none for expert missing (None, 5, 6, None, [averages])
+            mean_check = False
+            ''' distances will be a list of values which can contain None values. distances is a list and contains a None value then the expert count must 
+            increment
+            '''
+            if type(distances) is list and distances is not None:
+                for dist in distances:
+                    if not mean_check:
+                        expert_name = "Exp" + str(expert_count)
+                        distance_results[expert_name] = dist  # this could be a None value but is not an issue, expected as it will be evaluated later
+                        expert_count += 1
+                    else:
+                        distance_results["MeanExp"] = dist[0]
+                        distance_results["ExpMean"] = dist[1]
+                    if dist == "mean":  # this check will denote that the next entry is a 2 item list for the averages across the experts
+                        mean_check = True
+            else:
+                distance_results["Exp1"] = None
+                distance_results["MeanExp"] = None
+                distance_results["ExpMean"] = None
+            distance_results["Sample"] = sample_name
+            distance_results["Thresh"] = thresh_type
+            total_expert_count = max(total_expert_count, expert_count)  # remain unchanged after 1st thresh_type since expert count independent of thresh_type
+
+        return distance_results, total_expert_count
+
+    def _aggregate_across_samples(self, sample_values, evaluation_type=None):
+        '''
+        This method will aggregate all of the values by sample, the evaluation_type will determine the analysis for the per sample values (defaulted to None)
+        and organise these values for a pandas table which can be manipulated and output to csv.
+        :param sample_values:
+        :param evaluation_type:
+        :return:
+        '''
+        total_expert_count = 1  # this will monitor the number of experts to retroactively place None values for missing evaluations
+        aggregate_dict = {}
+        if evaluation_type is None:
+            for sample_name, sample_results in sample_values.items():
+                compared_values, expert_count = self._low_thresh_compare(sample_results, sample_name)  # make sure "Valid" will not be in the sample_results
+                for cv in list(compared_values):  # this will initialise the aggregate dictionary
+                    if cv not in aggregate_dict:
+                        aggregate_dict[cv] = []
 
     def analyze_low_thresholds(self, save_path=None):
         for f in self.file_list:
@@ -124,6 +250,6 @@ class thresholding_metrics(AutoThresholder):
 
 if __name__ == "__main__":
     input_path = ["C:\\RESEARCH\\Mitophagy_data\\Time_split\\Output\\"]
-    system_analyst = thresholding_metrics(input_path)
+    system_analyst = thresholding_metrics(input_path, expert_path="C:\\RESEARCH\\Mitophagy_data\\gui params\\")
     system_analyst.analyze_low_thresholds("C:\\RESEARCH\\Mitophagy_data\\Time_split\\System_metrics\\Low Threshold Metrics\\")
 
